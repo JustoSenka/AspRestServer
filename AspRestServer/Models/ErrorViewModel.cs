a1e1dffaadbffd8323461b5bf1ebba9cb9d11e55
Justas <glodjus@gmail.com>
2019-11-02 17:30:20 +0100

    New project created

---
+++ AspRestServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspRestServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

62d15ed60d816fc916493bd0fa4a7eb2004b747d
Justas <glodjus@gmail.com>
2019-11-05 21:04:59 +0100

    Renam AspRestServer to LanguageLearner

---
+++ LanguageLearner/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using LanguageLearner.Models;
+using LanguageLearner.Objects;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LanguageLearner.Controllers
+{
+    public class BookController : Controller
+    {
+        public BookModel bookModel;
+        public IActionResult Index()
+        {
+            bookModel = new BookModel() { LanguageFrom = new Language() { Name = "Spanish" } };
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

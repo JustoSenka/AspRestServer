1318f4b3c0139dcd176a36e794431fc5fdbdcee9
Justas <glodjus@gmail.com>
2020-01-05 18:47:03 +0100

    Making current nav item bold

--- LanguageLearner/wwwroot/js/site.js
+++ LanguageLearner/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+
+    highlightActiveMenuItem();
+    /*
+    $('ul.navbar-nav > li').click(function (e) {
+        $('ul.navbar-nav > li').removeClass('font-weight-bold');
+        $(this).addClass('font-weight-bold');
+    });*/
+});
+
+highlightActiveMenuItem = function () {
+    var url = window.location.pathname;
+
+    var controller = "/" + url.split("/")[1]; // etc:  /controller/action
+
+    $('.nav-item > a.nav-first[href="' + controller + '"]').addClass('font-weight-bold'); // Selects index actions by ctrl name
+    $('.nav-item > a.nav-first[href="' + url + '"]').addClass('font-weight-bold'); // Selects non index first level nav items like Privacy
+
+    $('.nav-item > a.nav-second[href="' + url + '"]').addClass('font-weight-bold'); // Selects second level nav fields by full url
+};

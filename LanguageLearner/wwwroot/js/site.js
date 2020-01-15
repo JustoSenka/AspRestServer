5610b275fcc87632b8ad6736d0cb96ccc552bbb9
Justas <glodjus@gmail.com>
2020-01-15 10:16:49 +0000

    [WIP] Adding scripts for searching functionality

--- LanguageLearner/wwwroot/js/site.js
+++ LanguageLearner/wwwroot/js/site.js
@@ -23,3 +23,36 @@ highlightActiveMenuItem = function () {
 
     $('.nav-item > a.nav-second[href="' + url + '"]').addClass('font-weight-bold'); // Selects second level nav fields by full url
 };
+
+
+// Searcheable dropdown
+ //run when the DOM is ready
+$(function () {                      
+    // Reset input if clicking on dropdown again
+    $('.searcheable-dropdown').click(function () {
+        var input = $('.searcheable-dropdown input');
+        input.val('');
+        filterFunction(input);
+    });
+    // Filtering function
+    $(".dropdown-menu input").keyup(function (e) {
+        filterFunction($(this));
+    });
+    $(".dropdown-menu input").click(function (e) {  //use a class, since your ID gets mangled
+        e.stopPropagation();   // do not close dropdown when clicking on search field
+    });
+});
+
+function filterFunction(obj) { // obj: JQuery<HTMLElement>
+    var filter = obj.val().toUpperCase();
+    var links = obj.parent().children('a');
+
+    for (var i = 0; i < links.length; i++) {
+        txtValue = links[i].textContent || links[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            links[i].style.display = "";
+        } else {
+            links[i].style.display = "none";
+        }
+    }
+}

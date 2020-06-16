3b4f18417c74795605d945c5a2a0d54ae74108dc
Justas <glodjus@gmail.com>
2020-06-16 22:46:05 +0200

    Adding NewTranslation view, moving common edit word functionality to shared layout

---
+++ Langs.Models/Entities/NewWordModel.cs
@@ -0,0 +1,10 @@
+﻿using Langs.Data.Objects;
+using Langs.Models.Shared;
+
+namespace Langs.Models
+{
+    public class NewWordModel : EditEntityModel, IAlertMessageModel
+    {
+        public int TranslationForID { get; set; }
+    }
+}

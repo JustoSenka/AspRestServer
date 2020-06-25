149341c9535b2d46db070b6a253df4b287a5592d
Justas <glodjus@gmail.com>
2020-06-25 21:32:57 +0300

    Adding practice controller view and model

---
+++ Langs.Controllers/PracticeController.cs
@@ -0,0 +1,34 @@
+﻿using Langs.Models.Practice;
+using Langs.Models.Words;
+using Langs.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Langs.Controllers
+{
+    public class PracticeController : BaseController
+    {
+        private readonly ILanguagesService LanguagesService;
+        private readonly IWordsService WordsService;
+        private readonly IBooksService BooksService;
+        private readonly IAccountService AccountService;
+        public PracticeController(IWordsService WordsService, ILanguagesService LanguagesService, IBooksService BooksService, IAccountService AccountService)
+        {
+            this.WordsService = WordsService;
+            this.LanguagesService = LanguagesService;
+            this.BooksService = BooksService;
+            this.AccountService = AccountService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = new PracticeModel
+            {
+                Books = BooksService.GetAllWithWordCount().Select(b => (b.ID, b.Name, b.WordCount)).ToArray(),
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Langs.Models/Practice/PracticeModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Langs.Models.Practice
+{
+    public class PracticeModel
+    {
+        public int SelectedBookID { get; set; }
+        public (int ID, string Name, int WordCount)[] Books { get; set; }
+        public (int Top, int Bottom) WordRange { get; set; }
+
+        /*
+
+        public int AccountID { get; set; }
+        public (int FromID, int ToID, string FromText, string ToText, string ToAlternateSpelling, string ToPronunciation)[] Words { get; set; }
+        public (int ID, string Name) Book { get; set; }
+
+    */
+    }
+}
